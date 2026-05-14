5549cbdb8758c0a4eb9964e76c1e69b97e0ac13f
yaron <ylapidot@gmail.com>
2026-05-14 21:27:04 +0300

    Server Fake

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port=5050;// הגדרת היציאה אליה יאזין השרת
 const srv=http.createServer(app);// יצירת אובייקט שרת
 srv.listen(port,()=>{
     console.log('Server Is Up');
-});    //  העלאת השרת לאוויר
+});    //     העלאת השרת לאוויר
